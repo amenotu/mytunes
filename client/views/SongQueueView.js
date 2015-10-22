34d62379e24532cc37918b16332a14d1c938bc9c
amenotu <kyabut@gmail.com>
2015-10-22 16:06:52 -0700

    SongQueue successfully renders updated collection

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -5,6 +5,10 @@ var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
     this.render();
+
+    this.collection.on('add', function(){
+      this.render();
+    }, this);
   },
 
   render: function() {
